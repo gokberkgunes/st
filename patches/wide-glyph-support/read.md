7b13d892dde18d2358fcb6d83635cd533dfc3f59
Gokberk Gunes <prhi2@tutanota.com>
2023-01-28 23:38:34 +0300

    wide glyph support patch for nvim plugins

---
+++ patches/wide-glyph-support/read.md
@@ -0,0 +1,5 @@
+Applying gide glyph support to simple terminal because logos do not appear in
+neovim plugings.
+
+I got the patch from following:
+https://st.suckless.org/patches/glyph_wide_support/
--- st.h
+++ st.h
@@ -37,6 +37,12 @@ enum glyph_attribute {
 	ATTR_SET        = 1 << 15, // max value
 };
 
+enum drawing_mode {
+    DRAW_NONE = 0,
+    DRAW_BG = 1 << 0,
+    DRAW_FG = 1 << 1,
+};
+
 enum selection_mode {
 	SEL_IDLE = 0,
 	SEL_EMPTY = 1,
